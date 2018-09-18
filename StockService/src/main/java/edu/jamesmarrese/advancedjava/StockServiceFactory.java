438c348c3ab9e770a9531eda8bcc84e20f9c4dd5
DESKTOP-RP113LT\James <jamesmarrese@yahoo.com>
2018-09-18 19:04:43 -0400

    another try

--- StockService/src/main/java/edu/jamesmarrese/advancedjava/StockServiceFactory.java
+++ StockService/src/main/java/edu/jamesmarrese/advancedjava/StockServiceFactory.java
@@ -39,7 +39,6 @@ public class StockServiceFactory implements StockService {
             return new StockQuote("AMZN", 150.75, date);
         if (symbol.equals("NFLX"))
             return new StockQuote("NFLX", 201.54, date);
-        return null;
     }
 
 }
